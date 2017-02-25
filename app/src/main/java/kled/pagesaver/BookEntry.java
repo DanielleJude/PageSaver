eb9039b3c0405f369ec12f40c0ab12e4b6a1c371
liu47y <liu47y@gmail.com>
2017-02-24 21:32:29 -0500

    Update BookEntry.java

--- app/src/main/java/kled/pagesaver/BookEntry.java
+++ app/src/main/java/kled/pagesaver/BookEntry.java
@@ -1,5 +1,11 @@
 package kled.pagesaver;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+
 /**
  * Created by eloisedietz on 2/24/17.
  */
@@ -13,6 +19,8 @@ public class BookEntry {
     private int mRating;
     private String mComment;
     private int mStatus;
+    private String mQuote;
+    private ArrayList<LatLng> mLocationList;
 
 
     public void setId(Long id){
@@ -71,6 +79,52 @@ public class BookEntry {
         return mStatus;
     }
 
+    public void setQuote(String quote) {
+        mQuote = quote;
+    }
+
+    public String getQuote() {
+        return mQuote;
+    }
+
+    public ArrayList<LatLng> getLocationList() {
+        return mLocationList;
+    }
+
+    // Convert Location ArrayList to byte array to store in SQLite database
+    public byte[] getLocationByteArray() {
+        int[] intArray = new int[mLocationList.size() * 2];
+
+        for (int i = 0; i < mLocationList.size(); i++) {
+            intArray[i * 2] = (int) (mLocationList.get(i).latitude * 1E6);
+            intArray[(i * 2) + 1] = (int) (mLocationList.get(i).longitude * 1E6);
+        }
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(intArray.length
+                * Integer.SIZE);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+        intBuffer.put(intArray);
+
+        return byteBuffer.array();
+    }
+
+    // Convert byte array to Location ArrayList
+    public void setLocationListFromByteArray(byte[] bytePointArray) {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytePointArray);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+
+        int[] intArray = new int[bytePointArray.length / Integer.SIZE];
+        intBuffer.get(intArray);
+
+        int locationNum = intArray.length / 2;
+
+        for (int i = 0; i < locationNum; i++) {
+            LatLng latLng = new LatLng((double) intArray[i * 2] / 1E6F,
+                    (double) intArray[i * 2 + 1] / 1E6F);
+            mLocationList.add(latLng);
+        }
+    }
 
 
 }
