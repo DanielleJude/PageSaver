4e6a14ee5cc42aae7ad4a75627fdbf998cb0cd80
liu47y <liu47y@gmail.com>
2017-02-26 13:19:57 -0500

    Update BookEntry.java
    Added registration id, phone id, list of start/end times, list of start/end pages, total page number

--- app/src/main/java/kled/pagesaver/BookEntry.java
+++ app/src/main/java/kled/pagesaver/BookEntry.java
@@ -4,6 +4,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.nio.LongBuffer;
 import java.util.ArrayList;
 
 /**
@@ -11,7 +12,9 @@ import java.util.ArrayList;
  */
 
 public class BookEntry {
-    private Long mId;
+    private Long mRowId;
+    private Long mRegId;   // registration ID
+    private Long mPhoneId; // unique phone ID
 
     private String mTitle;
     private String mAuthor;
@@ -21,14 +24,54 @@ public class BookEntry {
     private int mStatus;
     private String mQuote;
     private ArrayList<LatLng> mLocationList;
+    private ArrayList<StartEndTimes> mTimeList;
+    private ArrayList<StartEndPages> mPageList;
+    private int mTotalPages;   // number of total pages
 
+    /* List of duration ranges for each individual reading period */
+    public class StartEndTimes {
+        Long startTime;  // start times in milliseconds
+        Long endTime;    // end times in milliseconds
 
-    public void setId(Long id){
-        mId = id;
+        public StartEndTimes(Long mStartTime, Long mEndTime){
+            startTime = mStartTime;
+            endTime = mEndTime;
+        }
     }
 
-    public Long getId() {
-        return mId;
+    /* List of page ranges for each individual reading period */
+    public class StartEndPages {
+        int startPage;  // page number where this reading starts
+        int endPage;    // page number where this reading finishes
+
+        public StartEndPages(int mStartPage, int mEndPage){
+            startPage = mStartPage;
+            endPage = mEndPage;
+        }
+    }
+
+    public void setRowId(Long rowId){
+        mRowId = rowId;
+    }
+
+    public Long getRowId() {
+        return mRowId;
+    }
+
+    public void setRegId(Long regId){
+        mRegId = regId;
+    }
+
+    public Long getRegId() {
+        return mRegId;
+    }
+
+    public void setPhoneId(Long phoneId){
+        mPhoneId = phoneId;
+    }
+
+    public Long getPhoneId() {
+        return mPhoneId;
     }
 
     public void setTitle(String title) {
@@ -124,6 +167,90 @@ public class BookEntry {
                     (double) intArray[i * 2 + 1] / 1E6F);
             mLocationList.add(latLng);
         }
+    }
+
+    public ArrayList<StartEndTimes> getTimeList() {
+        return mTimeList;
+    }
+
+    // Convert start and end times ArrayList to byte array to store in SQLite database
+    public byte[] getTimeByteArray() {
+        long[] longArray = new long[mTimeList.size() * 2];
+
+        for (int i = 0; i < mTimeList.size(); i++) {
+            longArray[i * 2] = mTimeList.get(i).startTime;
+            longArray[(i * 2) + 1] = mTimeList.get(i).endTime;
+        }
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(longArray.length
+                * Integer.SIZE);
+        LongBuffer longBuffer = byteBuffer.asLongBuffer();
+        longBuffer.put(longArray);
+
+        return byteBuffer.array();
+    }
+
+    // Convert byte array to time ArrayList
+    public void setTimeListFromByteArray(byte[] bytePointArray) {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytePointArray);
+        LongBuffer longBuffer = byteBuffer.asLongBuffer();
+
+        long[] longArray = new long[bytePointArray.length / Integer.SIZE];
+        longBuffer.get(longArray);
+
+        int timeNum = longArray.length / 2;
+
+        for (int i = 0; i < timeNum; i++) {
+            StartEndTimes startEndTimes = new StartEndTimes(longArray[i * 2], longArray[i * 2 + 1]);
+            mTimeList.add(startEndTimes);
+        }
+    }
+
+    public ArrayList<StartEndPages> getPageList() {
+        return mPageList;
+    }
+
+    // Convert ArrayList for pages to byte array to store in SQLite database
+    public byte[] getPageByteArray() {
+        int[] intArray = new int[mPageList.size() * 2];
+
+        for (int i = 0; i < mPageList.size(); i++) {
+            intArray[i * 2] = mPageList.get(i).startPage;
+            intArray[(i * 2) + 1] = mPageList.get(i).endPage;
+        }
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(intArray.length
+                * Integer.SIZE);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+        intBuffer.put(intArray);
+
+        return byteBuffer.array();
+    }
+
+    // Convert byte array to ArrayList for pages
+    public void setPageListFromByteArray(byte[] bytePointArray) {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytePointArray);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+
+        int[] intArray = new int[bytePointArray.length / Integer.SIZE];
+        intBuffer.get(intArray);
+
+        int pageNum = intArray.length / 2;
+
+        for (int i = 0; i < pageNum; i++) {
+            StartEndPages startEndPages = new StartEndPages(intArray[i * 2], intArray[i * 2 + 1]);
+            mPageList.add(startEndPages);
+        }
+    }
+
+    public void setTotalPages(int totalPages) {
+        mTotalPages = totalPages;
+    }
+
+    public int getTotalPages() {
+        return mTotalPages;
     }
 
 
