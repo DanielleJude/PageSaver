897fa19cfb927c9a42ed12051c17cc98bc7a94a0
Eloise Dietz <eloise.r.dietz.17@dartmouth.edu>
2017-02-27 15:10:59 -0500

    delete Async Task

--- app/src/main/java/kled/pagesaver/ViewCurrentBookActivity.java
+++ app/src/main/java/kled/pagesaver/ViewCurrentBookActivity.java
@@ -1,6 +1,7 @@
 package kled.pagesaver;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,11 +12,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewCurrentBookActivity extends AppCompatActivity implements View.OnClickListener{
     public final static String ID_BUNDLE_KEY = "_idbundle key";
     private long mEntryId;
     private BookEntry entry;
+    private EntryDatastoreHelper datastoreHelper;
+    private BookEntryDbHelper database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,17 +77,50 @@ public class ViewCurrentBookActivity extends AppCompatActivity implements View.O
 
             case R.id.delete_menu_item:
                 //Delete from datastore
-                EntryDatastoreHelper datastoreHelper = new EntryDatastoreHelper(this);
-                datastoreHelper.deleteEntry(""+mEntryId);
+                datastoreHelper = new EntryDatastoreHelper(this);
+                toDelete delete = new toDelete();
+                delete.execute(mEntryId);
 
                 //Delete from local db
-                new BookEntryDbHelper(this).removeEntry(mEntryId);
+                database = new BookEntryDbHelper(this);
+                DeleteFromDatabase databaseDelete = new DeleteFromDatabase();
+                databaseDelete.execute(mEntryId);
                 finish();
                 break;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*
+AsyncTask to add an exercise entry to the database
+*/
+    private class toDelete extends AsyncTask<Long, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Long... params) {
+            datastoreHelper.deleteEntry(""+params[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void params){
+            Toast.makeText(getApplicationContext(), "Entry Deleted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class DeleteFromDatabase extends AsyncTask<Long, Void, Void> {
+        @Override
+        protected Void doInBackground(Long... params) {
+            database.removeEntry(params[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void params){
+        }
+    }
+
 
     public void onClick(View view) {
         int id = view.getId();
