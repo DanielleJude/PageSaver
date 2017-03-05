4431ebb1acb7a2f3adec0ddc81d2651a69d4ace3
kelleylin <kelley.lin5@gmail.com>
2017-03-04 19:35:30 -0500

    Blank CompareAnalyticsActivity

--- app/src/main/java/kled/pagesaver/AnalyticsActivity.java
+++ app/src/main/java/kled/pagesaver/AnalyticsActivity.java
@@ -1,12 +1,16 @@
 package kled.pagesaver;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -23,11 +27,11 @@ import lecho.lib.hellocharts.view.ColumnChartView;
 public class AnalyticsActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<ArrayList<BookEntry>> {
 
-    private ArrayList<Integer> hoursArray;
-    private ArrayList<Integer> monthsArray;
-    private ArrayList<Integer> pagesArray;
-    private ArrayList<Integer> durationArray;
-    private int[] pagesPerMonthsArray;
+    public ArrayList<Integer> hoursArray;
+    public ArrayList<Integer> monthsArray;
+    public ArrayList<Integer> pagesArray;
+    public ArrayList<Integer> durationArray;
+    public int[] pagesPerMonthsArray;
 
     LoaderManager loaderManager;
 
@@ -49,6 +53,25 @@ public class AnalyticsActivity extends AppCompatActivity implements
 
         loaderManager = this.getLoaderManager();
         loaderManager.initLoader(1, null, this);
+    }
+
+    // Toolbar to add
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.compare_analytics_menu, menu);
+        return true;
+    }
+
+    // Add Entry Page
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.compare_analytics:
+                Intent intent = new Intent(this, CompareAnalyticsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return true;
+        }
     }
 
     /**
---
+++ app/src/main/java/kled/pagesaver/CompareAnalyticsActivity.java
@@ -0,0 +1,20 @@
+package kled.pagesaver;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CompareAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_analytics);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
