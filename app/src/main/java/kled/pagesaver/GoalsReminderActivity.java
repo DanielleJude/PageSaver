21ea92cbf69bf633ba737d04195b35f688af7e9c
LilyLiu <liu47y@gmail.com>
2017-03-07 20:38:49 -0500

    Update goal reminder

--- app/src/main/java/kled/pagesaver/GoalsReminderActivity.java
+++ app/src/main/java/kled/pagesaver/GoalsReminderActivity.java
@@ -1,9 +1,12 @@
 package kled.pagesaver;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -37,6 +40,35 @@ public class GoalsReminderActivity extends AppCompatActivity implements
         ListView entryView = (ListView) findViewById(R.id.goals_reminder_list);
         goalsReminderAdapter = new GoalsReminderAdapter(this, entries);
         entryView.setAdapter(goalsReminderAdapter);
+
+        entryView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), ViewGoalActivity.class);
+                GoalEntry viewedEntry = entries.get(position);
+
+                intent.putExtra(GoalsDbHelper.KEY_ID, viewedEntry.getId());
+                intent.putExtra(GoalsDbHelper.KEY_TITLE, viewedEntry.getBookTitle());
+                intent.putExtra(GoalsDbHelper.KEY_DESCRIPTION, viewedEntry.getDescription());
+                intent.putExtra(GoalsDbHelper.KEY_GOALS_PAGES,
+                        Integer.toString(viewedEntry.getPagesToComplete()));
+                intent.putExtra(GoalsDbHelper.KEY_PAGES_INCREMENT,
+                        Integer.toString(viewedEntry.getDailyPages()));
+                intent.putExtra(GoalsDbHelper.KEY_PROGRESS,
+                        Integer.toString(viewedEntry.getReadPages()));
+                intent.putExtra(GoalsDbHelper.KEY_GOAL_START,
+                        Integer.toString(viewedEntry.getGoalStartPage()));
+                intent.putExtra(GoalsDbHelper.KEY_GOAL_END,
+                        Integer.toString(viewedEntry.getGoalEndPage()));
+
+                if (viewedEntry.getEndTime() != null) {
+                    intent.putExtra(GoalsDbHelper.KEY_END_TIME, viewedEntry.getEndTime());
+                }
+
+                startActivity(intent);
+            }
+        });
     }
 
     /**
